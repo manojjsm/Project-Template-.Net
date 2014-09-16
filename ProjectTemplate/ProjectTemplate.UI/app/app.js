ae12c1c7bb5ec3c1ebc0c3df612181de227e2e9e
rcadaoas <romeo.mail7@gmail.com>
2014-09-16 10:35:34 +0400

    configure angular modules in app.js

--- ProjectTemplate/ProjectTemplate.UI/app/app.js
+++ ProjectTemplate/ProjectTemplate.UI/app/app.js
@@ -1,1 +1,38 @@
-﻿
+﻿/*this file will be responsible for creating modules in our applications.
+we can consider module as a collection of services, directives and filters which is used
+by our application*/
+
+//initializing our angular modules
+var app = angular.module('AngularAuthApp',['ngRoute','LocalStorageModule','angular-loading-bar']);
+
+//configuration our routes
+app.config(function ($routeProvider) {
+    
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/app/views/orders.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+
+}]);
