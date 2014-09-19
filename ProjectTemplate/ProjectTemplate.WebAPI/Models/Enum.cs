8795114d3c9b36edb9dfaa1b17967cc92a803f72
rcadaoas <romeo.mail7@gmail.com>
2014-09-19 23:17:12 +0400

    client, refresh token CLR and Enum class

---
+++ ProjectTemplate/ProjectTemplate.WebAPI/Models/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate.WebAPI.Models
+{
+    public class Enum
+    {
+        public enum ApplicationTypes
+        {
+            JavaScript = 0,
+            NativeConfidential = 1
+        };
+    }
+}
