aad243d52c6e1cfd5b78e6262ac805db5e1913c0
rcadaoas <romeo.mail7@gmail.com>
2014-09-20 14:19:38 +0400

    server authorization provider

--- ProjectTemplate/ProjectTemplate.WebAPI/Providers/SimpleAuthorizationServerProvider.cs
+++ ProjectTemplate/ProjectTemplate.WebAPI/Providers/SimpleAuthorizationServerProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security.OAuth;
+using ProjectTemplate.WebAPI.Entities;
 using ProjectTemplate.WebAPI.Repository;
 using System;
 using System.Collections.Generic;
@@ -21,6 +22,66 @@ namespace ProjectTemplate.WebAPI.Providers
         public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
             context.Validated();
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string clientId = string.Empty;
+            string clientSecret = string.Empty;
+            Client client = null;
+        
+            if(!context.TryGetBasicCredentials(out clientId, out clientSecret)
+            {
+                context.TryGetFormCredentials(out clientId, out clientSecret);
+            }
+
+            if(context.ClientId == null)
+            {
+                context.Validated();
+                //context.SetError("invalid_clientId","ClientId should be sent.");
+                return Task.FromResult<object>(null);
+
+            }
+
+            using(AuthRepository _repo = new AuthRepository())
+            {
+                client = _repo.FindClient(context.ClientId);
+            }
+
+            if(client == null)
+            {
+                context.SetError("invalid_clientId", string.Format("Client '{0}' is not registered in the system.", context.ClientId));
+                return Task.FromResult<object>(null);
+            }
+
+            if(client.ApplicationType == Models.Enum.ApplicationTypes.NativeConfidential)
+            {
+                if(string.IsNullOrWhiteSpace(clientSecret))
+                {
+                    context.SetError("invalid_clientId","Client secret is invalid.");
+                    return Task.FromResult<object>(null);
+                }
+                else
+                {
+                    if(client.Secret != Helper.GetHash(clientSecret))
+                    {
+                        context.SetError("invalid_clientId","Client secret is invalid.");
+                        return Task.FromResult<object>(null);
+                    }
+                }
+            }
+
+            if(!client.Active)
+            {
+                context.SetError("invalid_clientId","Client is inactive.");
+                return Task.FromResult<object>(null);
+            }
+
+            context.OwinContext.Set<string>("as:clientAllowedOrigin", client.AllowedOrigin);
+            context.OwinContext.Set<string>("as:clientRefreshTokenLifeTime",client.RefreshTokenLifeTime.ToString());
+
+            context.Validated();
+            return Task.FromResult<object>(null);
         }
 
         /// <summary>
