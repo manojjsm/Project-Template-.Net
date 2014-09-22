c97a57d59ef03de81be985d8f545bc1bc7d7c74e
rcadaoas <romeo.mail7@gmail.com>
2014-09-22 15:20:45 +0400

    Helper class

---
+++ ProjectTemplate/ProjectTemplate.WebAPI/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate.WebAPI
+{
+    public class Helper
+    {
+        public static string GetHash(string input)
+        {
+            HashAlgorithm hashAlgorithm = new SHA256CryptoServiceProvider();
+
+            byte[] byteValue = System.Text.Encoding.UTF8.GetBytes(input);
+
+            byte[] byteHash = hashAlgorithm.ComputeHash(byteValue);
+
+            return Convert.ToBase64String(byteHash);
+        }
+    }
+}
